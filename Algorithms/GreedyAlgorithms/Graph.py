620a3f2aa9209c59d5178f9cb63b9a2bf16a0480
shamanyu <shubham.s@greyorange.sg>
2017-08-29 12:47:34 +0530

    Make folder for greedy algorithms, begin implementing a basic Graph and add file for Kruskals

---
+++ Algorithms/GreedyAlgorithms/Graph.py
@@ -0,0 +1,112 @@
+class Vertex(object):
+
+  def __init__(self, name):
+    self.set_vertex(name)
+
+  def get_user_input(self):
+    name = input()
+    self.set_vertex(name)
+
+  def set_vertex(self, name):
+    self.name = name
+
+  def get_vertex(self):
+    return self.name
+
+class Edge(object):
+
+  def __init__(self, source_vertex, destination_vertex):
+    self.set_edge(source_vertex, destination_vertex)
+
+  def get_user_input(self):
+    [source_vertex, destination_vertex] = [Vertex(vertex_name) for 
+      vertex_name in input().split()]
+    self.set_edge(source_vertex, destination_vertex)
+
+  def set_edge(self, source_vertex, destination_vertex):
+    self.source_vertex = source_vertex
+    self.destination_vertex = destination_vertex
+    self.edge = (source_vertex, destination_vertex)
+
+  def get_edge(self):
+    return self.edge
+
+  def get_source_vertex(self):
+    return self.source_vertex
+
+  def get_destination_vertex(self):
+    return self.destination_vertex
+
+class Graph(object):
+
+  def __init__(self):
+    self.set_input(list(), list())
+
+  def get_user_input(self):
+    vertices = [Vertex(vertex_name) for vertex_name in input.split()]
+    edge_details = [edge_detail for edge in input.split()]
+    edges = list()
+    for edge_detail in edge_details:
+      (source_vertex_name, destination_vertex_name) = edge_detail
+      source_vertex = Vertex(source_vertex_name)
+      destination_vertex = Vertex(destination_vertex_name)
+      edges.append(Edge(source_vertex, destination_vertex))
+    self.set_input(vertices, edges)
+
+  def set_input(self, vertices, edges):
+    # import pdb; pdb.set_trace()
+    self.number_of_vertices = 0
+    self.number_of_edges = 0
+    self.vertices = list()
+    self.edges = list()
+    for vertex in vertices:
+      self.add_vertex(vertex)
+    for edge in edges:
+      self.add_edge(edge)
+
+  def add_vertex(self, vertex):
+    self.vertices.append(vertex)
+    self.number_of_vertices += 1
+
+  def remove_vertex(self, vertex):
+    try:
+      self.vertices.remove(vertex)
+    except ValueError:
+      pass
+    self.number_of_vertices -= 1
+
+  def add_edge(self, edge):
+    source_vertex = edge.get_source_vertex()
+    destination_vertex = edge.get_destination_vertex()
+    if source_vertex in self.vertices and destination_vertex in self.vertices:
+      self.edges.append(edge)
+      self.number_of_edges += 1
+
+  def remove_edge(self, edge):
+    try:
+      self.edges.remove(edge)
+    except ValueError:
+      pass
+    self.number_of_edges -= 1
+
+  def get_graph(self):
+    return [self.vertices, self.edges]
+
+  def print_graph(self):
+    print ("\nVertices are: ")
+    for vertex in self.vertices:
+      print (vertex.get_vertex(), end=' ')
+    print ("\nEdges are: ")
+    for edge in self.edges:
+      print(edge.get_edge(), end=' ')
+    print("\n")
+
+vertex_a = Vertex('a')
+vertex_b = Vertex('b')
+
+edge_1 = Edge(vertex_a, vertex_b)
+edge_2 = Edge(vertex_b, vertex_a)
+
+graph = Graph()
+graph.set_input([vertex_a, vertex_b], [edge_1, edge_2])
+graph.print_graph()
---
+++ Algorithms/GreedyAlgorithms/Kruskals.py
@@ -0,0 +1,6 @@
+# https://en.wikipedia.org/wiki/Kruskal%27s_algorithm
+
+class Kruskal(object):
+
+  def __init__(self):
+
