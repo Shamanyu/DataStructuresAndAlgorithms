0b58d51ddc961f2d4c8348bb52a07ac7f546175e
shamanyu <shubham.s@greyorange.sg>
2017-09-07 09:58:04 +0530

    Add functions to get size, check if empty, rotate and print queue

--- DataStructures/Queue/Queue.py
+++ DataStructures/Queue/Queue.py
@@ -20,6 +20,28 @@ class Queue(object):
     except IndexError:
       return None
 
+  def size(self):
+    return len(self.elements)
+
+  def is_empty(self):
+    return self.size() == 0
+
+  def rotate(self, number_of_operations):
+    for counter in range(0, number_of_operations):
+      element = self.pop()
+      self.push(element)
+
+  def print_queue(self):
+    print (self.elements)
+
+# queue = Queue()
+# queue.push(1)
+# queue.push(2)
+# queue.push(3)
+# queue.push(4)
+# queue.rotate(5)
+# queue.print_queue()
+
 # queue = Queue()
 # queue.push(1)
 # print (queue.top())
@@ -41,3 +63,16 @@ class Queue(object):
 # print (queue.top())
 # print (queue.pop())
 # print (queue.top())
+# queue.push(7)
+# print (queue.size())
+# queue.push(8)
+# print (queue.size())
+# queue.push(9)
+# print (queue.size())
+# print (queue.is_empty())
+# queue.pop()
+# print (queue.is_empty())
+# queue.pop()
+# print (queue.is_empty())
+# queue.pop()
+# print (queue.is_empty())
