4ebbbbb9e443c49e6868ee5f2511c0780ac9ae1b
shamanyu <shubhamshamanyu@gmail.com>
2018-03-28 05:30:26 +0530

    Add boiler plate code for searchInsertPosition problem of Leetcode

---
+++ LeetCode/SearchInsertPosition/search_insert_position/search_insert_position.py
@@ -0,0 +1,23 @@
+class SearchInsertPosition(object):
+
+  def searchInsertPosition(self):
+    pass
+
+
+from nose.tools import assert_equals, assert_raises
+
+class TestSearchInsertPosition(object):
+
+  def testSearchInsertPosition(self):
+    searchInsertPosition = SearchInsertPosition()
+
+    print ("All test cases passed!")
+
+
+def main():
+  testSearchInsertPosition = TestSearchInsertPosition()
+  testSearchInsertPosition.testSearchInsertPosition()
+
+if __name__ == '__main__':
+  main()
+    
