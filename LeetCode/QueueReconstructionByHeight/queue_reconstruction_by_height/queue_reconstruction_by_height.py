df6e456ad4b485a4488385191c854020dc7edc71
shamanyu <shubhamshamanyu@gmail.com>
2018-04-26 12:34:23 +0530

    Add test cases for QueueReconstructionByHeight problem of LeetCode

---
+++ LeetCode/QueueReconstructionByHeight/queue_reconstruction_by_height/queue_reconstruction_by_height.py
@@ -0,0 +1,22 @@
+class QueueReconstructionByHeight(object):
+
+    def queueReconstructionByHeight(self):
+        pass
+
+
+from nose.tools import assert_equals, assert_raises
+
+class TestQueueReconstructionByHeight(object):
+
+  def testQueueReconstructionByHeight(self):
+    queueReconstructionByHeight = QueueReconstructionByHeight()
+
+    print ("All test cases passed!")
+
+
+def main():
+  testQueueReconstructionByHeight = TestQueueReconstructionByHeight()
+  testQueueReconstructionByHeight.testQueueReconstructionByHeight()
+
+if __name__ == '__main__':
+  main()
