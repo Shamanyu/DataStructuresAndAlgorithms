6a0ccf14327c9d2fe8406a52b5bd60ec59df98e7
shamanyu <shubhamshamanyu@gmail.com>
2018-04-27 14:15:42 +0530

    Optimize MostFrequentSubtreeSum problem of LeetCode

--- LeetCode/MostFrequentSubtreeSum/most_frequent_subtree_sum/most_frequent_subtree_sum.py
+++ LeetCode/MostFrequentSubtreeSum/most_frequent_subtree_sum/most_frequent_subtree_sum.py
@@ -9,19 +9,40 @@ class TreeNode(object):
 
 class MostFrequentSubtreeSum(object):
 
-    def __init__(self):
-        self.sumDict = defaultdict(int)
+    # def __init__(self):
+    #     self.sumDict = defaultdict(int)
+    #
+    # def findFrequentTreeSum(self, root):
+    #     self._calculateSubTreeSum(root)
+    #     return [key for key,value in self.sumDict.items() if value == max(self.sumDict.values())]
+    #
+    # def _calculateSubTreeSum(self, root):
+    #     if root is None:
+    #         return 0
+    #     sum = root.val + self._calculateSubTreeSum(root.left) + self._calculateSubTreeSum(root.right)
+    #     self.sumDict[sum] += 1
+    #     return sum
 
+    # Optimal solution
     def findFrequentTreeSum(self, root):
-        self._calculateSubTreeSum(root)
-        return [key for key,value in self.sumDict.items() if value == max(self.sumDict.values())]
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
 
-    def _calculateSubTreeSum(self, root):
-        if root is None:
-            return 0
-        sum = root.val + self._calculateSubTreeSum(root.left) + self._calculateSubTreeSum(root.right)
-        self.sumDict[sum] += 1
-        return sum
+        self.ans = defaultdict(int)
+        self.findTreeSum(root)
+        most_freq = max(self.ans.values())
+        return [x for x in self.ans if self.ans[x] == most_freq]
+
+    def findTreeSum(self, root):
+        res = (root.val
+               + (0 if not root.left else self.findTreeSum(root.left))
+               + (0 if not root.right else self.findTreeSum(root.right)))
+        self.ans[res] += 1
+        return res
 
 
 from nose.tools import assert_equals, assert_raises
