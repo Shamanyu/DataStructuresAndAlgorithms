bdfe3aed813b7653cf72fdcdb41e14d0801ed712
shamanyu <shubhamshamanyu@gmail.com>
2018-04-27 12:39:03 +0530

    Solve QueueReconstructionByHeight problem of LeetCode

--- LeetCode/QueueReconstructionByHeight/queue_reconstruction_by_height/queue_reconstruction_by_height.py
+++ LeetCode/QueueReconstructionByHeight/queue_reconstruction_by_height/queue_reconstruction_by_height.py
@@ -1,7 +1,11 @@
 class QueueReconstructionByHeight(object):
 
-    def queueReconstructionByHeight(self):
-        pass
+    def queueReconstructionByHeight(self, people):
+        people.sort(key=lambda a:[a[0], -a[1]], reverse=True)
+        result = list()
+        for person in people:
+            result.insert(person[1], person)
+        return result
 
 
 from nose.tools import assert_equals, assert_raises
@@ -10,6 +14,10 @@ class TestQueueReconstructionByHeight(object):
 
   def testQueueReconstructionByHeight(self):
     queueReconstructionByHeight = QueueReconstructionByHeight()
+
+    assert_equals(queueReconstructionByHeight.queueReconstructionByHeight(
+        [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
+    ),  [[5, 0], [7, 0], [5, 2], [6, 1], [4, 4], [7, 1]])
 
     print ("All test cases passed!")
 
