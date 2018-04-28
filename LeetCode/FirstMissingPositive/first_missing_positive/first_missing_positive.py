0b5228186a2175d66ee9aac13f5171b717d3d587
shamanyu <shubhamshamanyu@gmail.com>
2018-04-28 11:45:02 +0530

    Solve FirstMissingPositive problem of LeetCode

--- LeetCode/FirstMissingPositive/first_missing_positive/first_missing_positive.py
+++ LeetCode/FirstMissingPositive/first_missing_positive/first_missing_positive.py
@@ -2,17 +2,38 @@ class FirstMissingPositive(object):
 
   def firstMissingPositive(self, nums):
 
-    # nums.append(0)
-    # n = len(nums)
-    # for i in range(len(nums)): #delete those useless elements
-    #     if nums[i]<0 or nums[i]>=n:
-    #         nums[i]=0
-    # for i in range(len(nums)): #use the index as the hash to record the frequency of each number
-    #     nums[nums[i]%n]+=n
-    # for i in range(1,len(nums)):
-    #     if nums[i]/n==0:
-    #         return i
-    pass
+    # Partition array in place around 0
+    leftPointer = 0
+    rightPointer = len(nums) - 1
+    pivot = 0
+    while (rightPointer >= leftPointer):
+        if nums[rightPointer] > pivot and nums[leftPointer] <= pivot:
+            nums[rightPointer], nums[leftPointer] = nums[leftPointer], nums[rightPointer]
+            leftPointer += 1
+            rightPointer -= 1
+        elif nums[rightPointer] <= pivot and nums[leftPointer] > pivot:
+            leftPointer += 1
+            rightPointer -= 1
+        elif nums[rightPointer] > pivot:
+            leftPointer += 1
+        elif nums[leftPointer] <= pivot:
+            rightPointer -= 1
+
+    partitionPosition = rightPointer+1
+
+    # Use array to maintain elements present
+    for counter in range(0, partitionPosition):
+        position = abs(nums[counter])-1
+        if position < len(nums):
+            if nums[position] >= 0:
+                nums[position] *= -1
+
+    # Find first smallest positive number that doesn't exist
+    for counter in range(0, partitionPosition):
+        if nums[counter] > 0:
+            return counter+1
+
+    return partitionPosition+1
 
 from nose.tools import assert_equals, assert_raises
 
@@ -24,6 +45,10 @@ class TestFirstMissingPositive(object):
     assert_equals(firstMissingPositive.firstMissingPositive([1, 2, 0]), 3)
 
     assert_equals(firstMissingPositive.firstMissingPositive([3, 4, -1, 1]), 2)
+
+    assert_equals(firstMissingPositive.firstMissingPositive([7, 8, 9, 11, 12]), 1)
+
+    assert_equals(firstMissingPositive.firstMissingPositive([1, 1]), 2)
 
     print ("All test cases passed!")
 
